09062a389759f954172942fc347993fe3850b043
Isaac Longoria <isaac.longoria9136@gmail.com>
2026-02-04 16:31:38 -0600

    Removed google anlaytics tags from root

--- app/layout.tsx
+++ app/layout.tsx
@@ -131,20 +131,6 @@ export default function RootLayout({
           {children}
           <CenteredWithLogo />
         </RoleModalProvider>
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-        />
-        <Script id="ga4" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
-              page_path: window.location.pathname,
-            });
-          `}
-        </Script>
         {gtmId ? (
           <noscript>
             <iframe
